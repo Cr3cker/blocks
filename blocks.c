733740e482fd6167fbb182946a66f5af0487c136
Vladislav <vladstar1606@mail.ru>
2024-01-22 19:22:45 +0100

    Add basis for multiple blocks and bonuses

--- blocks.c
+++ blocks.c
@@ -13,7 +13,7 @@
 #define BRICK_HEIGHT 20
 #define VOLUME 150.0
 #define COLORS 7
-
+#define INIT_NUM_BALLS 1
 
 typedef struct {
     Vector2 pos;
@@ -37,6 +37,9 @@ typedef struct {
 } Brick;
 
 Brick matrix[10][10];
+
+Ball* balls = NULL;
+int num_balls = 0;
 
 Color MY_RED = { 230, 41, 55, 255 };
 Color MY_ORANGE = { 255, 161, 0, 255 };
@@ -86,7 +89,21 @@ void start_game(Sound start_sound) {
     }
 }
 
-void init_game(Player *player, Ball *ball, Sound start_sound, bool full_restart) {
+
+void initialize_balls(Player player) {
+    num_balls = INIT_NUM_BALLS;
+    balls = malloc(num_balls * sizeof(Ball));
+    if (balls == NULL) {
+        exit(1);
+    }
+
+    balls[0].pos = (Vector2){ .x = player.pos.x + 35, .y = player.pos.y - 20 };
+    balls[0].radius = 10.0;
+    balls[0].active = false;
+    balls[0].speed = (Vector2){ .x = 0, .y = -5 };
+}
+
+void init_game(Player *player, Sound start_sound, bool full_restart) {
     start_game(start_sound);
 
     float x = 0; 
@@ -117,28 +134,37 @@ void init_game(Player *player, Ball *ball, Sound start_sound, bool full_restart)
     player->pos.y = WINDOW_HEIGHT - 200 - PLAYER_HEIGHT / 2;
     player->is_game_over = false;
 
-    ball->radius = 10.0;
-    ball->active = false;
-    ball->pos.x = WINDOW_WIDTH / 2;
-    ball->pos.y = WINDOW_HEIGHT / 2;
-    ball->speed.x = 0;
-    ball->speed.y = -5;
+    initialize_balls(*player);
 }
 
-void activate_ball(Ball *ball) {
-    ball->pos.x += ball->speed.x;
-    ball->pos.y += ball->speed.y;
+void activate_ball(Ball *balls) {
+    if (IsKeyPressed(KEY_SPACE) && balls[0].active == false) {
+        balls[0].active = true;
+    }
+
+    if (balls[0].active) {
+        balls[0].pos.x += balls[0].speed.x;
+        balls[0].pos.y += balls[0].speed.y;
+    }
 }
 
-void move_right(Player *player) {
+void move_right(Player *player, Ball *balls) {
     player->pos.x += 10;
+
+    if (!balls[0].active) {
+        balls[0].pos.x += 10;
+    }
 }
 
-void move_left(Player *player) {
+void move_left(Player *player, Ball *balls) {
     player->pos.x -= 10;
+    
+    if (!balls[0].active) {
+        balls[0].pos.x -= 10;
+    }
 }
 
-void update_game(Player player, Ball ball, Texture2D heart, Texture2D loser, Sound win_sound, Music background_music) {
+void update_game(Player player, Ball *balls, Texture2D heart, Texture2D loser, Sound win_sound, Music background_music) {
     if (!player.is_game_over && player.lives > 0) {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
@@ -147,8 +173,13 @@ void update_game(Player player, Ball ball, Texture2D heart, Texture2D loser, Sou
                 }
             }
         }
+
         DrawRectangle(player.pos.x, player.pos.y, PLAYER_WIDTH, PLAYER_HEIGHT, BLACK);
-        DrawCircle(ball.pos.x, ball.pos.y, ball.radius, BLACK);
+
+
+        for (int i = 0; i < num_balls; i++) {
+            DrawCircle(balls[i].pos.x, balls[i].pos.y, balls[i].radius, BLACK);
+        }
 
         int tex_x = 10;
         int tex_y = WINDOW_HEIGHT - 100;
@@ -188,60 +219,51 @@ void update_game(Player player, Ball ball, Texture2D heart, Texture2D loser, Sou
     }
 }
 
-void detect_collisions(Ball *ball, Player *player, Sound deep, Music background_music, Sound start_sound) {
-    if (ball->pos.y == 0) {
-        ball->speed.y *= -1;
-    }
 
-    if (ball->pos.y == WINDOW_HEIGHT - 50 && !player->is_game_over) {
-        player->lives -= 1;
-        if (player->lives > 0) {
-            init_game(player, ball, start_sound, false);
-        } else {
-            StopMusicStream(background_music);
-            PlaySound(deep);
+void detect_collisions(Ball *balls, Player *player, Sound deep, Music background_music, Sound start_sound) {
+
+    for (int i = 0; i < num_balls; i++) {
+        if (balls[i].pos.y == 0) {
+            balls[i].speed.y *= -1;
         }
-    }
 
-    if (ball->pos.x <= 10 || ball->pos.x >= WINDOW_WIDTH - 10) {
-        ball->speed.x *= -1;
-    }
+        if (balls[i].pos.y == WINDOW_HEIGHT - 50 && !player->is_game_over) {
+            player->lives -= 1;
+            if (player->lives > 0) {
+                init_game(player, start_sound, false);
+            } else {
+                StopMusicStream(background_music);
+                PlaySound(deep);
+            }
+        }
 
-    Rectangle rec = {player->pos.x, player->pos.y, PLAYER_WIDTH, PLAYER_HEIGHT};
-    if (CheckCollisionCircleRec(ball->pos, ball->radius, rec)) {
-        ball->speed.y *= -1;
-        ball->speed.x = (ball->pos.x - player->pos.x)/(PLAYER_WIDTH / 2)*5;
-    }
+        if (balls[i].pos.x <= 10 || balls[i].pos.x >= WINDOW_WIDTH - 10) {
+            balls[i].speed.x *= -1;
+        }
 
-    bool hasCollidedWithBrick = false;
+        Rectangle rec = {player->pos.x, player->pos.y, PLAYER_WIDTH, PLAYER_HEIGHT};
+        if (CheckCollisionCircleRec(balls[i].pos, balls[i].radius, rec)) {
+            balls[i].speed.y *= -1;
+            balls[i].speed.x = (balls[i].pos.x - player->pos.x)/(PLAYER_WIDTH / 2)*5;
+        }
 
-    for (int i = 0; i < 10; i++) {
+        bool hasCollidedWithBrick = false;
+
         for (int j = 0; j < 10; j++) {
-            Rectangle brick = {matrix[i][j].pos.x, matrix[i][j].pos.y, BRICK_WIDTH, BRICK_HEIGHT};
-            if (CheckCollisionCircleRec(ball->pos, ball->radius, brick) && matrix[i][j].is_visible) {
-                matrix[i][j].is_visible = false;
-                if (!hasCollidedWithBrick) {
-                    ball->speed.y *= -1;
-                    hasCollidedWithBrick = true;
+            for (int k = 0; k < 10; k++) {
+                Rectangle brick = {matrix[j][k].pos.x, matrix[j][k].pos.y, BRICK_WIDTH, BRICK_HEIGHT};
+                if (CheckCollisionCircleRec(balls[i].pos, balls[i].radius, brick) && matrix[j][k].is_visible) {
+                    matrix[j][k].is_visible = false;
+                    if (!hasCollidedWithBrick) {
+                        balls[i].speed.y *= -1;
+                        hasCollidedWithBrick = true;
+                    }
                 }
             }
         }
     }
 }
 
-bool check_win() {
-    bool won = true;
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            if (matrix[i][j].is_visible) {
-                won = false;
-            }
-        }
-    }
-
-    return won;
-}
 
 int main() {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Gachi Blocks");
@@ -273,9 +295,8 @@ int main() {
     SetTargetFPS(60);
 
     Player player = { 0 };
-    Ball ball = { 0 };
 
-    init_game(&player, &ball, start_sound, true);
+    init_game(&player, start_sound, true);
 
     PlayMusicStream(background_music);
 
@@ -284,24 +305,24 @@ int main() {
 
         ClearBackground((Color){200, 200, 200, 255});
         
-        activate_ball(&ball);
+        activate_ball(balls);
 
         UpdateMusicStream(background_music);
 
         if (IsKeyPressed(KEY_ENTER)) {
-            init_game(&player, &ball, start_sound, true);
+            init_game(&player, start_sound, true);
             player.is_game_over = false;
             PlayMusicStream(background_music);
         }
 
         if (IsKeyDown(KEY_LEFT) && player.pos.x > 10) {
-            move_left(&player);
+            move_left(&player, balls);
         } else if (IsKeyDown(KEY_RIGHT) && player.pos.x < WINDOW_WIDTH - 80) {
-            move_right(&player);
+            move_right(&player, balls);
         }
 
-        detect_collisions(&ball, &player, lose_sound, background_music, start_sound);
-        update_game(player, ball, texture_heart, texture_loser, win_sound, background_music);
+        detect_collisions(balls, &player, lose_sound, background_music, start_sound);
+        update_game(player, balls, texture_heart, texture_loser, win_sound, background_music);
 
         EndDrawing();
     }
